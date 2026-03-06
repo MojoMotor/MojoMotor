1b2715a42195fb61d66edd72e874d4457b901bd7
George Stephanis <daljo628@gmail.com>
2026-03-05 23:26:15 -0500

    Use archived MojoMotor docs URLs
    Replace references to mojomotor.com user guide, forums, bug tracker and license pages with corresponding https://web.archive.org/... archived URLs across the codebase. Changes touch import templates, user guide HTML, controller/library/helper/JS headers, language strings and inline comments to preserve/document links now that the original site is unavailable.

--- system/mojomotor/views/welcome/welcome_message.php
+++ system/mojomotor/views/welcome/welcome_message.php
@@ -2,6 +2,6 @@
 
 <h2><?=$page_title?></h2>
 
-<p>Welcome! Just getting everything settled in? If you're looking to install me, take a look at the <a href="http://mojomotor.com/user_guide/setup/index.html">installation section</a> of the <a href="http://mojomotor.com/user_guide/">MojoMotor User Guide</a> and then visit <?= anchor('setup', 'the installer wizard')?>.</p>
+<p>Welcome! Just getting everything settled in? If you're looking to install me, take a look at the <a href="https://web.archive.org/web/20120919080359/http://mojomotor.com/user_guide/setup/index.html">installation section</a> of the <a href="https://web.archive.org/web/20120919080359/http://mojomotor.com/user_guide/">MojoMotor User Guide</a> and then visit <?= anchor('setup', 'the installer wizard')?>.</p>
 
 <?php $this->load->view('mojo_footer');?>
